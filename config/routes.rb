a718773088b83f80e805c9b4216106856c063622
paulogrupp <paulo.rupp198@gmail.com>
2022-08-19 18:42:26 -0300

    make it look like dontpad

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/notepads/:name', to: 'notepads#edit'
-  put '/notepads/:name', to: 'notepads#update'
+  get '/:name', to: 'notepads#edit'
+  put '/:name', to: 'notepads#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
