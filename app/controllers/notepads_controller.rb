5b37fa42aa22a198c046c01ab1b169f39ddcb79b
paulogrupp <paulo.rupp198@gmail.com>
2022-09-01 13:31:49 -0300

    make it look like it should

--- app/controllers/notepads_controller.rb
+++ app/controllers/notepads_controller.rb
@@ -1,45 +1,11 @@
 class NotepadsController < ApplicationController
-  before_action :set_notepad, only: %i[ show edit update destroy ]
+  before_action :set_notepad, only: %i[edit update]
+  def edit; end
 
-  # GET /notepads or /notepads.json
-  def index
-    @notepads = Notepad.all
-  end
-
-  # GET /notepads/1 or /notepads/1.json
-  def show
-  end
-
-  # GET /notepads/new
-  def new
-    @notepad = Notepad.new
-  end
-
-  # GET /notepads/1/edit
-  def edit
-  end
-
-  # POST /notepads or /notepads.json
-  def create
-    @notepad = Notepad.new(notepad_params)
-
-    respond_to do |format|
-      if @notepad.save
-        format.html { redirect_to notepad_url(@notepad), notice: "Notepad was successfully created." }
-        format.json { render :show, status: :created, location: @notepad }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @notepad.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /notepads/1 or /notepads/1.json
   def update
     respond_to do |format|
       if @notepad.update(notepad_params)
-        format.html { redirect_to notepad_url(@notepad), notice: "Notepad was successfully updated." }
-        format.json { render :show, status: :ok, location: @notepad }
+        format.json { render body: nil, status: :ok }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @notepad.errors, status: :unprocessable_entity }
@@ -47,24 +13,15 @@ class NotepadsController < ApplicationController
     end
   end
 
-  # DELETE /notepads/1 or /notepads/1.json
-  def destroy
-    @notepad.destroy
+  private
 
-    respond_to do |format|
-      format.html { redirect_to notepads_url, notice: "Notepad was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_notepad
+    @notepad = Notepad.find_or_initialize_by(name: params[:name])
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_notepad
-      @notepad = Notepad.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def notepad_params
-      params.require(:notepad).permit(:text, :name)
-    end
+  # Only allow a list of trusted parameters through.
+  def notepad_params
+    params.permit(:text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :notepads
+  get '/:name', to: 'notepads#edit', as: :notepads_edit
+  put '/:name', to: 'notepads#update', as: :notepads
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
